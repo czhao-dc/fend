c18413c02eda522180a71e408cb1f7671048115e
czhao-dc <64813364+czhao-dc@users.noreply.github.com>
2023-09-07 01:31:43 -0700

    add files for the Evaluate a News Article with Natural Language Processing project

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,16 +1,78 @@
 function handleSubmit(event) {
     event.preventDefault()
-
+    console.log('cakked')
     // check what text was put into the form field
     let formText = document.getElementById('name').value
-    checkForName(formText)
+    Client.checkForName(formText)
 
     console.log("::: Form Submitted :::")
-    fetch('http://localhost:8080/test')
-    .then(res => res.json())
-    .then(function(res) {
-        document.getElementById('results').innerHTML = res.message
+    // fetch('http://localhost:8081/test')
+    // .then(res => res.json())
+    // .then(function(res) {
+    //     document.getElementById('results').innerHTML = res.message
+    // })
+    getData(formText)
+    .then(function(data){
+        // postData('/addEntry', {polarity:data.score_tag,subjectivity: data.subjectivity, text: formText});
+        postData('/addEntry', {polarity:data.score_tag,subjectivity: data.subjectivity, text: formText});
     })
+
+    return "success"
+    // .then(
+
+    //     updateUI()
+    // )
+}
+
+
+
+const getData = async (text)=>{
+    var FormData = require('form-data');
+    const formdata = new FormData();
+
+    formdata.append("key", "fa2a1d580f8b1e0d5a64ef9ea77d9aef");
+    formdata.append("txt", text);
+    formdata.append("lang", "en");
+    const requestOptions = {
+        method: 'POST',
+        body: formdata,
+        redirect: 'follow'
+      };
+    
+
+    const res = await fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions);
+    try {
+  
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+
+
+const postData = async ( url = '', data = {})=>{
+    
+    const response = await fetch(url, {
+    method: 'POST', // *GET, POST, PUT, DELETE, etc.
+    credentials: 'same-origin', 
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data), // body data type must match "Content-Type" header        
+  });
+
+    try {
+      const newData = await response.json();
+            console.log(newData)
+            document.getElementById('results').innerHTML = 'polarity: '+newData[newData.length-1].polarity+', subjectivity: '+newData[newData.length-1].subjectivity+', text: '+newData[newData.length-1].text;
+             return newData
+    }catch(error) {
+    console.log("error", error);
+    // appropriately handle the error
+    }
 }
 
 export { handleSubmit }
--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,3 +1,8 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
+const projectData = [];
+
 var path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
@@ -5,19 +10,63 @@ const mockAPIResponse = require('./mockAPI.js')
 const app = express()
 
 app.use(express.static('dist'))
-
+console.log(`Your API key is ${process.env.API_KEY}`);
 console.log(__dirname)
 
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//const cors = require('cors');
+//app.use(cors());
+
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+    res.sendFile('dist/index.html')
+    //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
+app.get('/all', function (req, res) {
+    console.log('sent from all',projectData
+    )
+    res.send(projectData)
+  })
+
+
+
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(8081, function () {
+    console.log('Example app listening on port 8081!')
 })
+
+app.post('/addEntry', addProjectData);
+
+function addProjectData (req, res) {
+
+
+    let polarity = ''
+    if (req.body.polarity == 'P+' | req.body.polarity == 'P') {polarity = 'positive'}
+    else if (req.body.polarity == 'NEU') {polarity = 'neutral'}
+    else if (req.body.polarity == 'N+' | req.body.polarity == 'N') {polarity = 'negative'}
+    else {polarity = 'without polarity'} 
+
+    newEntry = {
+        polarity: polarity,
+        subjectivity: req.body.subjectivity,
+        text: req.body.text,
+
+    }
+    projectData.push(newEntry);
+    console.log('this is', projectData[projectData.length-1])
+    res.send(projectData)
+    
+    
+};
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
+})
+
+
+app.get('/test1', function (req, res) {
+    res.send('kkkk')
 })
