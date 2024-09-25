94294744c9e857f5968e5421c1b9c3c28667622d
czhao-dc <64813364+czhao-dc@users.noreply.github.com>
2024-09-25 05:06:16 -0700

    implement essential functionalities (todo: webpack, service workers, and an extra functionality/feature)

--- projects/capstone/website/app.js
+++ projects/capstone/website/app.js
@@ -1,8 +1,9 @@
 /* Global Variables */
-const apiKey = '13c2d4a41a2205d07ea0069f0320e129&units=imperial';
+//const apiKey = '13c2d4a41a2205d07ea0069f0320e129&units=imperial';
 // const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=94040,us&appid={API key}';
-const baseURL = 'https://api.openweathermap.org/data/2.5/weather?zip=';
+// const baseURL = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=';
 
+const baseURL = 'http://localhost:3000/'
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
@@ -12,29 +13,64 @@ let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 document.getElementById('generate').addEventListener('click', performAction);
 
 function performAction(e){
-const newZip =  document.getElementById('zip').value;
-const newContent =  document.getElementById('feelings').value;
+    const city =  document.getElementById('zip').value;
+    const country =  document.getElementById('feelings').value;
 
-getWeather(baseURL,newZip, apiKey)
-.then(function(data){
-    console.log(data.main.temp);
-    console.log(newContent);
-    console.log(newDate);
-    postData('/addEntry', {temp: data.main.temp, content: newContent, date: newDate});
-})
-.then(
-    updateUI()
-)
+    const departureDate = document.getElementById('departingDate').value
+
+    function convertHTMLDateToJSDate(htmlDate) {
+      // Assuming htmlDate is in the format 'YYYY-MM-DD'
+      const [year, month, day] = htmlDate.split('-').map(Number);
+    
+      // JavaScript months are 0-indexed, so subtract 1 from the month
+      return new Date(year, month - 1, day);
+    }
+
+    const departingDateJS = convertHTMLDateToJSDate(departureDate)
+
+    function getDifferenceInDays(futureDate) {
+      const today = new Date();
+      console.log(today,futureDate);
+      const timeDifference = futureDate.getTime() - today.getTime();
+      const daysDifference = Math.ceil(timeDifference / (1000 * 3600 * 24));
+    
+      return daysDifference;
+    }
+
+    const daysFromToday = getDifferenceInDays(departingDateJS);
+
+    getCoordinate(city,country)
+    // .then(function(data){
+    //     console.log(data);
+    //     // console.log(newContent);
+    //     // console.log(newDate);
+    //     postData('/addEntry', {lng: data.lng, lat: data.lat});
+    // })
+    .then(function(data) {
+      return getWeather(data.lat,data.lng, daysFromToday)
+    }).then((weatherData) =>  {
+      console.log(weatherData);
+      updateUI(daysFromToday, weatherData)
+    }
+  ).then(
+      getPicture(city+', '+country)
+
+    )
+
 
 }
 
-const getWeather = async (baseURL, zip, key)=>{
+const getCoordinate = async (city,country)=>{
 
     //const res = await fetch(baseURL+zip+key)
-    const res = await fetch(baseURL+zip+',us&appid='+key);
+    const res = await fetch(baseURL+`getCoordinate?city=${city}&country=${country}`);
     try {
   
       const data = await res.json();
+      localStorage.setItem('lat', JSON.stringify(data.lat));
+      localStorage.setItem('lng', JSON.stringify(data.lng));
+      console.log(data.lat,data.lng);
+      console.log(data);
       return data;
     }  catch(error) {
       console.log("error", error);
@@ -43,17 +79,67 @@ const getWeather = async (baseURL, zip, key)=>{
   }
 
 
+  const getWeather = async (lat,lng, days)=>{
 
-const updateUI = async () =>{
-    const request = await fetch('/all');
+    //const res = await fetch(baseURL+zip+key)
+    const res = await fetch(baseURL+`getWeather?lat=${lat}&lng=${lng}&days=${days}`);
     try {
+  
+      const data = await res.json();
+      console.log(data)
+      return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+
+
+  const getPicture = async (place)=>{
+
+    //const res = await fetch(baseURL+zip+key)
+    const res = await fetch(baseURL+`getPicture?place=${place}`);
+    try {
+  
+      const data = await res.json();
+      document.getElementById('picture').innerHTML='<img src="' +data["hits"][0]["largeImageURL"]+'"/>';
+      // return data;
+    }  catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+
+
+const updateUI = async (day, weather) =>{
+    // const request = await fetch('/all');
+    try {
+      console.log(weather);
+      const dataID = day % 7;
+      console.log(dataID);
+
+      if (day > 7) {
+        let high_temp = weather['data'][dataID]['max_temp']
+        let low_temp = weather['data'][dataID]['low_temp']
+        let weather_description = weather['data'][dataID]['weather']['description']
+        var weather_str = 'High: ' + high_temp + ' Low: ' + low_temp + ' Weather: ' + weather_description;
+      } else {
+
+        let temp = weather['data'][0]['temp']
+        let weather_description = weather['data'][0]['weather']['description']
+        var weather_str = 'Temperature: ' + temp + ' Weather: ' + weather_description;
+      }
     // Transform into JSON
-    const allData = await request.json()
-    console.log(allData)
-    // Write updated data to DOM elements
-    document.getElementById('temp').innerHTML = Math.round(allData[0].temp)+ 'degrees';
-    document.getElementById('content').innerHTML = allData[0].content;
-    document.getElementById('date').innerHTML =allData[0].date;
+    // const allData = await request.json()
+    // console.log(allData)
+    // // Write updated data to DOM elements
+    // document.getElementById('temp').innerHTML = Math.round(allData[0].temp)+ 'degrees';
+    // document.getElementById('content').innerHTML = allData[0].content;
+    // document.getElementById('date').innerHTML =allData[0].date;
+    // document.getElementById('picture').innerHTML=url
+      document.getElementById('temp').innerHTML = 'Your trip is ' + day + ' days from today'
+      // document.getElementById('date').innerHTML = 'High: ' + high_temp + ' Low: ' + low_temp + ' Weather: ' + weather_description;
+      document.getElementById('date').innerHTML = weather_str;
     }
     catch(error) {
       console.log('error', error);
